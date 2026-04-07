f4479a3e6d005aac8b00fd76cd7592cf82ce1616
Om Prakash <omprakashshekhar2@gmail.com>
2026-04-07 22:44:14 +0530

    updated url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "import.meta.env.VITE_API_URL" ||"https://condom-life-backend.onrender.com/api",
+  baseURL: "https://condom-life-backend.onrender.com/api/",
   timeout: 10000,
   headers: { "Content-Type": "application/json" },
 });
